aa640b014eafac038a6c31a9deb1d9ada5d7c2fd
Marniks7 <marniks7>
2021-06-22 15:05:13 -0400

    Add count to bitmap stats

--- benchmark/Prices-487k-PricesPerOffering-9.7k/stats-roaring-benchmark_test.go
+++ benchmark/Prices-487k-PricesPerOffering-9.7k/stats-roaring-benchmark_test.go
@@ -26,29 +26,37 @@ func TestBitmap_CalculateAndPrintRoaringStats(t *testing.T) {
 	var bSum uint64
 	var sbSum uint64
 
+	var cnt int
 	_, b, sb := benchmark.PrintBitmapStats(f, ind.Index.CharBitmaps, "CharBitmaps")
 	bSum += b
 	sbSum += sb
+	cnt += len(ind.Index.CharBitmaps)
 	_, b, sb = benchmark.PrintBitmapStats(f, ind.Index.SpecBitmaps, "SpecBitmaps")
 	bSum += b
 	sbSum += sb
+	cnt += len(ind.Index.SpecBitmaps)
 	_, b, sb = benchmark.PrintBitmapStats(f, ind.Index.GroupBitmaps, "GroupBitmaps")
 	bSum += b
 	sbSum += sb
+	cnt += len(ind.Index.GroupBitmaps)
 	_, b, sb = benchmark.PrintBitmapStats(f, ind.Index.OfferingBitmaps, "OfferingBitmaps")
 	bSum += b
 	sbSum += sb
+	cnt += len(ind.Index.OfferingBitmaps)
 	_, b, sb = benchmark.PrintBitmapStats(f, ind.Index.CharValuesBitmaps, "CharValuesBitmaps")
 	bSum += b
 	sbSum += sb
+	cnt += len(ind.Index.CharValuesBitmaps)
 	arr := make([]*roaring.Bitmap, 1)
 	arr[0] = ind.Index.DefaultBitmaps
 	_, b, sb = benchmark.PrintBitmapStats(f, arr, "DefaultBitmaps")
 	bSum += b
 	sbSum += sb
+	cnt += 1
 	fmt.Fprintln(f, "============================== Total ==============================================")
 	fmt.Fprintf(f, "Size: %v\n", benchmark.ConvertToHumanReadableSizeUint64(bSum))
 	fmt.Fprintf(f, "Serialized Size: %v\n", benchmark.ConvertToHumanReadableSizeUint64(sbSum))
+	fmt.Fprintf(f, "Count: %v\n", cnt)
 }
 
 func prepareBitmapIndexT(t *testing.T) (*cache.CatalogService, *indexroaring.BitmapIndexService) {
