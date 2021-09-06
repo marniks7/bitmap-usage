16f970adc4c3aee21c45c84ff883d8fa761777d6
Marniks7 <9289172+marniks7@users.noreply.github.com>
2021-09-06 00:07:45 -0400

    generate wrk requests, remove unused field from single request

--- benchmark/500k-large-groups/sample/generate-find-price-json.go
+++ benchmark/500k-large-groups/sample/generate-find-price-json.go
@@ -2,13 +2,39 @@ package sample
 
 import (
 	"bitmap-usage/model"
+	"bytes"
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
+	"os"
 	"strconv"
 )
 
-func GenerateJson(size int) {
+func GenerateJsonRequest(size int) {
+	file, filename := GenerateJsonAndReturn(size)
+
+	_ = ioutil.WriteFile(filename+".json", file, 0644)
+}
+
+func GenerateWrkRequest(size int) {
+	file, err := os.ReadFile("wrk-json-report-sample.lua")
+	if err != nil {
+		panic(err)
+	}
+	buffer := bytes.NewBuffer(file)
+	buffer.Write([]byte("\nwrk.method = \"POST\"\n"))
+	buffer.Write([]byte("wrk.headers[\"Content-Type\"] = \"application/json\"\n"))
+	file, filename := GenerateJsonAndReturn(size)
+
+	buffer.Write([]byte("wrk.body   = "))
+	buffer.WriteString(strconv.Quote(string(file)))
+	_ = ioutil.WriteFile("wrk-"+filename+".lua", buffer.Bytes(), 0644)
+}
+
+func GenerateJsonAndReturn(size int) ([]byte, string) {
+	if size > 65535 {
+		panic("bulk for >65535 items are not allowed for performance reasons")
+	}
+
 	frb := make([]model.FindPriceRequestBulk, 0, size)
 	termValues := ValuePool["Term"]
 	max := len(termValues)
@@ -20,22 +46,23 @@ func GenerateJson(size int) {
 				{"VPN", "5739614e-6c52-402c-ba3a-534c51b3201a"},
 				{"Router", "Not Included"}}})
 	}
+
 	var file []byte
 	var err error
 	var filename string
 	if len(frb) == 1 {
-		file, err = json.Marshal(frb[0])
-		filename = "search-price-request.json"
+		bulk := frb[0]
+		file, err = json.Marshal(model.FindPriceRequest{GroupId: bulk.GroupId,
+			OfferingId: bulk.OfferingId, PriceSpecId: bulk.PriceSpecId,
+			CharValues: bulk.CharValues})
+		filename = "search-price-request"
 	} else {
 		file, err = json.Marshal(frb)
-		filename = "search-price-bulk-request-" + strconv.Itoa(size) + ".json"
+		filename = "search-price-bulk-request-" + strconv.Itoa(size)
 	}
 
 	if err != nil {
-		fmt.Println("Unable to encode object", err)
-		return
+		panic(err)
 	}
-
-	_ = ioutil.WriteFile(filename, file, 0644)
-
+	return file, filename
 }
--- go.mod
+++ go.mod
@@ -5,11 +5,10 @@ go 1.16
 require (
 	github.com/RoaringBitmap/roaring v0.9.1
 	github.com/andybalholm/brotli v1.0.3 // indirect
-	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/davecgh/go-spew v1.1.1
 	github.com/dgraph-io/sroar v0.0.0-20210624194305-2cc5980b720b
 	github.com/gofiber/adaptor/v2 v2.1.8
 	github.com/gofiber/fiber/v2 v2.14.0
-	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
 	github.com/google/uuid v1.3.0
 	github.com/gorilla/mux v1.8.0
 	github.com/jinzhu/copier v0.3.2
@@ -21,7 +20,7 @@ require (
 	github.com/stretchr/testify v1.7.0
 	github.com/ugorji/go/codec v1.2.6
 	github.com/valyala/fasthttp v1.28.0
-	github.com/wcharczuk/go-chart/v2 v2.1.0 // indirect
+	github.com/wcharczuk/go-chart/v2 v2.1.0
 	golang.org/x/image v0.0.0-20210628002857-a66eb6448b8d // indirect
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c
 	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
